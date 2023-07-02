f07e27166fab8aa653743b1e7ffe768c751f7610
Stephane Charette <stephane.charette@hank.ai>
2023-07-02 01:21:09 -0700

    skip the attempt to load data/labels/*.png images (alphabet?)

--- src/image.c
+++ src/image.c
@@ -265,18 +265,24 @@ void draw_bbox(image a, box bbox, int w, float r, float g, float b)
 
 image **load_alphabet()
 {
-    int i, j;
-    const int nsize = 8;
-    image** alphabets = (image**)xcalloc(nsize, sizeof(image*));
-    for(j = 0; j < nsize; ++j){
-        alphabets[j] = (image*)xcalloc(128, sizeof(image));
-        for(i = 32; i < 127; ++i){
-            char buff[256];
-            sprintf(buff, "data/labels/%d_%d.png", i, j);
-            alphabets[j][i] = load_image_color(buff, 0, 0);
-        }
-    }
-    return alphabets;
+	const int nsize = 8;
+	image** alphabets = (image**)xcalloc(nsize, sizeof(image*));
+	for(int j = 0; j < nsize; ++j)
+	{
+		alphabets[j] = (image*)xcalloc(128, sizeof(image));
+		for(int i = 32; i < 127; ++i)
+		{
+#if 0
+			/// @todo What is this function doing?  What images are we trying to load here, and why?
+			char buff[256];
+			sprintf(buff, "data/labels/%d_%d.png", i, j);
+			alphabets[j][i] = load_image_color(buff, 0, 0);
+#else
+			alphabets[j][i] = make_image(0, 0, 3);
+#endif
+		}
+	}
+	return alphabets;
 }
 
 
