90cc7b1f9a385068fd1f90d88648dafa3688847c
Jason Daniel <83985724+lordofkillz@users.noreply.github.com>
2023-11-07 23:57:36 -0500

    Update CM_dependencies.cmake

--- CM_dependencies.cmake
+++ CM_dependencies.cmake
@@ -52,41 +52,37 @@ ENDIF ()
 # == cuDNN ==
 # ===========
 IF (DARKNET_USE_CUDA)
-	IF (WIN32)
-		# If installed according to the NVIDIA instructions, CUDNN should look like this:
-		#		C:\Program Files\NVIDIA\CUDNN\v8.x\...
-		# The .dll is named:
-		#		C:\Program Files\NVIDIA\CUDNN\v8.x\bin\cudnn64_8.dll
-		# And the header should look like:
-		#		C:\Program Files\NVIDIA\CUDNN\v8.x\include\cudnn.h
-		#
-		SET (CUDNN_DIR "C:/Program Files/NVIDIA/CUDNN/v8.x")
-		SET (CUDNN_DLL "${CUDNN_DIR}/bin/cudnn64_8.dll")
-		SET (CUDNN_LIB "${CUDNN_DIR}/lib/x64/cudnn.lib")
-		SET (CUDNN_HEADER "${CUDNN_DIR}/include/cudnn.h")
-		IF (EXISTS ${CUDNN_DLL} AND EXISTS ${CUDNN_LIB} AND EXISTS ${CUDNN_HEADER})
-			MESSAGE (STATUS "cuDNN found at ${CUDNN_DIR}")
-			INCLUDE_DIRECTORIES (${CUDNN_DIR}/include/)
-			ADD_COMPILE_DEFINITIONS (ABC_123_TESTING)
-			ADD_COMPILE_DEFINITIONS (CUDNN) # TODO this needs to be renamed
-			ADD_COMPILE_DEFINITIONS (CUDNN_HALF)
-			SET (DARKNET_LINK_LIBS ${DARKNET_LINK_LIBS} ${CUDNN_LIB})
-		ELSE ()
-			MESSAGE (WARNING "Did not find cuDNN at ${CUDNN_DIR}")
-		ENDIF ()
-	ELSE ()
-		# Should be slightly easier to deal with on Linux if it was installed correctly.
-		FIND_LIBRARY (CUDNN cudnn OPTIONAL QUIET)
-		IF (NOT CUDNN)
-			MESSAGE (STATUS "Skipping cuDNN")
-		ELSE ()
-			MESSAGE (STATUS "Enabling cuDNN")
-			ADD_COMPILE_DEFINITIONS (CUDNN) # TODO this needs to be renamed
-			ADD_COMPILE_DEFINITIONS (CUDNN_HALF)
-			SET (DARKNET_LINK_LIBS ${DARKNET_LINK_LIBS} ${CUDNN})
-		ENDIF ()
-	ENDIF ()
-ENDIF ()
+    IF (WIN32)
+        # Automatically detect cuDNN for Windows
+        FIND_PATH(CUDNN_INCLUDE cudnn.h HINTS ${CUDAToolkit_INCLUDE_DIRS} PATH_SUFFIXES cuda include)
+        FIND_LIBRARY(CUDNN_LIBRARY NAMES cudnn64_8 cudnn HINTS ${CUDAToolkit_LIBRARY_DIR} PATH_SUFFIXES bin lib/x64)
+
+        IF (CUDNN_INCLUDE AND CUDNN_LIBRARY)
+            MESSAGE(STATUS "cuDNN found.")
+            INCLUDE_DIRECTORIES(${CUDNN_INCLUDE})
+            ADD_COMPILE_DEFINITIONS(CUDNN) # TODO: Rename this to DARKNET_USE_CUDNN
+            ADD_COMPILE_DEFINITIONS(CUDNN_HALF)
+            SET(DARKNET_LINK_LIBS ${DARKNET_LINK_LIBS} ${CUDNN_LIBRARY})
+        ELSE()
+            MESSAGE(WARNING "cuDNN not found.")
+        ENDIF()
+    ELSE()
+        # Automatically detect cuDNN for Linux
+        FIND_PATH(CUDNN_INCLUDE cudnn.h HINTS ${CUDAToolkit_INCLUDE_DIRS} PATH_SUFFIXES cuda include)
+        FIND_LIBRARY(CUDNN_LIBRARY NAMES cudnn HINTS ${CUDAToolkit_LIBRARY_DIR} PATH_SUFFIXES lib lib64)
+
+        IF (CUDNN_INCLUDE AND CUDNN_LIBRARY)
+            MESSAGE(STATUS "cuDNN found.")
+            INCLUDE_DIRECTORIES(${CUDNN_INCLUDE})
+            ADD_COMPILE_DEFINITIONS(CUDNN) # TODO: Rename this to DARKNET_USE_CUDNN
+            ADD_COMPILE_DEFINITIONS(CUDNN_HALF)
+            SET(DARKNET_LINK_LIBS ${DARKNET_LINK_LIBS} ${CUDNN_LIBRARY})
+        ELSE()
+            MESSAGE(STATUS "Skipping cuDNN")
+        ENDIF()
+    ENDIF()
+ENDIF()
+
 
 
 # ========================
