e5ee0b94cd686c131674a14342ecc8633dafa874
Stéphane Charette <stephanecharette@gmail.com>
2024-03-18 08:35:21 -0700

    started cleaning up of box.* based on timing+tracking results

--- src-lib/box.hpp
+++ src-lib/box.hpp
@@ -10,15 +10,6 @@ typedef struct{
     float dx, dy, dw, dh;
 } dbox;
 
-//typedef struct detection {
-//	box bbox;
-//	int classes;
-//	float *prob;
-//	float *mask;
-//	float objectness;
-//	int sort_class;
-//} detection;
-
 typedef struct detection_with_class {
 	detection det;
 	// The most probable class id: the best class index in this->prob.
@@ -26,12 +17,9 @@ typedef struct detection_with_class {
 	int best_class;
 } detection_with_class;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 box float_to_box(float *f);
 box float_to_box_stride(float *f, int stride);
-float box_iou(box a, box b);
+float box_iou(const box & a, const box & b);
 float box_iou_kind(box a, box b, IOU_LOSS iou_kind);
 float box_rmse(box a, box b);
 dxrep dx_box_iou(box a, box b, IOU_LOSS iou_loss);
@@ -48,7 +36,3 @@ box encode_box(box b, box anchor);
 // Creates array of detections with prob > thresh and fills best_class for them
 // Return number of selected detections in *selected_detections_num
 detection_with_class* get_actual_detections(detection *dets, int dets_num, float thresh, int* selected_detections_num, char **names);
-
-#ifdef __cplusplus
-}
-#endif
