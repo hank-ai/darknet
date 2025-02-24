dd5141bf618e3db60d590932c21ed886428d7610
Stéphane Charette <stephanecharette@gmail.com>
2025-02-24 14:07:16 -0800

    added wording about multiple GPUs

--- src-lib/darknet_args_and_parms.cpp
+++ src-lib/darknet_args_and_parms.cpp
@@ -164,7 +164,7 @@ const Darknet::SArgsAndParms & Darknet::get_all_possible_arguments()
 		ArgsAndParms("extoutput"			),
 		ArgsAndParms("savelabels"			),
 		ArgsAndParms("chart"				),
-		ArgsAndParms("gpus"                 , "", " "	, "The index of which GPUs to use.  --gpus 0"							    ),
+		ArgsAndParms("gpus"					, "", " "	, "The index of the GPU to use. Multiple GPUs can be specified, such as -gpus 0,1"),
 	};
 
 	return all;
