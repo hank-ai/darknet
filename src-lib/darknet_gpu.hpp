9a6da62d4bc7a7261fe7925cbd45c7d479919e30
Stéphane Charette <stephanecharette@gmail.com>
2025-03-09 23:13:04 -0700

    ROCm: defines needed for OpenMI (issue #99)

--- src-lib/darknet_gpu.hpp
+++ src-lib/darknet_gpu.hpp
@@ -37,6 +37,7 @@
 #include <rocrand/rocrand.h>
 #include <hiprand/hiprand.h>
 #include <hipblas/hipblas.h>
+#include <miopen/miopen.h>
 
 #define SHFL_DOWN(val, offset) shfl_xor(val, offset)
 #define CUDA_SUCCESS hipSuccess
@@ -118,8 +119,46 @@
 #define curandSetPseudoRandomGeneratorSeed hiprandSetPseudoRandomGeneratorSeed
 #define curandCreateGenerator hiprandCreateGenerator
 
-// cuDNN -> hipDNN
-#define cudnnHandle_t hipdnnHandle_t
+// cuDNN -> miopen
+#define cudnnHandle_t miopenHandle_t
+#define cudnnStatus_t miopenStatus_t
+#define cudnnDataType_t miopenDataType_t
+#define cudnnDataType_t miopenDataType_t
+#define CUDNN_STATUS_SUCCESS miopenStatus_t::miopenStatusSuccess
+#define CUDNN_DATA_HALF miopenDataType_t::miopenHalf
+#define CUDNN_DATA_FLOAT miopenDataType_t::miopenFloat
+#define CUDNN_BATCHNORM_SPATIAL miopenBatchNormMode_t::miopenBNSpatial
+#define CUDNN_CROSS_CORRELATION miopenConvolutionMode_t::miopenConvolution
+#define cudnnTensorDescriptor_t miopenTensorDescriptor_t
+#define cudnnFilterDescriptor_t miopenTensorDescriptor_t
+#define cudnnPoolingDescriptor_t miopenPoolingDescriptor_t
+#define cudnnConvolutionDescriptor_t miopenConvolutionDescriptor_t
+#define cudnnConvolutionFwdAlgo_t miopenConvFwdAlgorithm_t
+#define cudnnConvolutionBwdDataAlgo_t miopenConvBwdDataAlgorithm_t
+#define cudnnConvolutionBwdFilterAlgo_t miopenConvBwdWeightsAlgorithm_t
+#define cudnnDestroyTensorDescriptor miopenDestroyTensorDescriptor
+#define cudnnCreateTensorDescriptor miopenCreateTensorDescriptor
+#define cudnnBatchNormalizationForwardTraining miopenBatchNormalizationForwardTraining
+#define cudnnBatchNormalizationBackward miopenBatchNormalizationBackward
+#define cudnnCreateConvolutionDescriptor miopenCreateConvolutionDescriptor
+#define cudnnCreateFilterDescriptor miopenCreateTensorDescriptor /* TODO: check this, does tensor replace filter? */
+#define cudnnCreate miopenCreate
+#define cudnnConvolutionMode_t miopenConvolutionMode_t
+#define cudnnSetStream miopenSetStream
+
+// MIOpen doesn't have the NCHW parameter; need to go from 7 parms down to 6 parms
+#define CUDNN_TENSOR_NCHW 0 // we'll ignore this parameter, so give it a dummy value
+#define cudnnSetTensor4dDescriptor(d, i, t, b, c, h, w) miopenSet4dTensorDescriptor(d, t, b, c, h, w)
+#define cudnnSetFilter4dDescriptor(d, t, i, n, c, h, w) miopenSet4dTensorDescriptor(d, t, n, c, h, w)
+
+// Parms are in a different order
+#define cudnnSetConvolution2dDescriptor(d, h, w, u, v, x, y, mode)									miopenInitConvolutionDescriptor(d, mode, h, w, u, v, x, y)
+#define cudnnConvolutionForward(h, alpha, xd, x, wd, w, cd, algo, ws, s, beta, yd, y)				miopenConvolutionForward(h, alpha, xd, x, wd, w, cd, algo, beta, yd, y, ws, s)
+#define cudnnConvolutionBackwardData(h, alpha, wd, w, dyd, dy, cd, algo, ws, s, beta, dxd, dx)		miopenConvolutionBackwardData(h, alpha, dyd, dy, wd, w, cd, algo, beta, dxd, dx, wd, s)
+#define cudnnConvolutionBackwardFilter(h, alpha, xd, x, dyd, dy, cd, algo, ws, s, beta, dwd, dw)	miopenConvolutionBackwardWeights(h, alpha, dyd, dy, xd, x, cd, algo, beta, dwd, dw, ws, s)
+#define cudnnGetConvolutionForwardWorkspaceSize(h, xd, wd, cd, yd, algo, s)							miopenConvolutionForwardGetWorkSpaceSize(h, wd, xd, cd, yd, s)
+#define cudnnGetConvolutionBackwardFilterWorkspaceSize(h, xd, dyd, cd, gd, algo, s)					miopenConvolutionBackwardWeightsGetWorkSpaceSize(h, dyd, xd, cd, dwd, s)
+#define cudnnGetConvolutionBackwardDataWorkspaceSize(h, wd, dyd, cd, dxd, algo, s)					miopenConvolutionBackwardDataGetWorkSpaceSize(h, dyd, wd, cd, dxd, s)
 
 #endif
 // === END OF AMD ROCm ===
