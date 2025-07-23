5da219713f6d0620ddd8606091d8565401d138ac
Stéphane Charette <stephanecharette@gmail.com>
2025-07-23 00:57:33 -0700

    parse .cfg and output a node in the onnx file for each layer

--- src-onnx/darknet_onnx_export.cpp
+++ src-onnx/darknet_onnx_export.cpp
@@ -24,17 +24,32 @@ int main(int argc, char * argv[])
 {
 	try
 	{
+		// make sure the network loads correctly
 		Darknet::Parms parms	= Darknet::parse_arguments(argc, argv);
-		Darknet::NetworkPtr net	= Darknet::load_neural_network(parms);
-		const auto cfg_fn		= Darknet::get_config_filename(net);
-		const auto weights_fn	= Darknet::get_weights_filename(net);
+		Darknet::set_verbose(false);
+		Darknet::NetworkPtr ptr	= Darknet::load_neural_network(parms);
+		const auto cfg_fn		= Darknet::get_config_filename(ptr);
+		const auto weights_fn	= Darknet::get_weights_filename(ptr);
 		const auto onnx_fn		= std::filesystem::path(cfg_fn).replace_extension(".onnx");
+		Darknet::free_neural_network(ptr);
 
-		std::cout << std::endl << "Darknet/YOLO ONNX Export Tool"		<< std::endl
+		std::cout														<< std::endl
+			<< "Darknet/YOLO ONNX Export Tool"							<< std::endl
 			<< "-> configuration ........ " << cfg_fn		.string()	<< std::endl
 			<< "-> weights .............. " << weights_fn	.string()	<< std::endl
 			<< "-> onnx output .......... " << onnx_fn		.string()	<< std::endl
 			;
+
+		// force the verbose logging to get the colour output when the network is parsed
+		Darknet::set_verbose(true);
+
+		// the following block of code is taken from load_network_custom() and parse_network_cfg_custom()
+		Darknet::Network * net = (Darknet::Network*)xcalloc(1, sizeof(Darknet::Network));
+		Darknet::CfgFile cfg(cfg_fn);
+		*net = cfg.create_network(1, 1);
+		load_weights(net, weights_fn.string().c_str());
+		fuse_conv_batchnorm(*net);
+		calculate_binary_weights(net);
 
 		GOOGLE_PROTOBUF_VERIFY_VERSION;
 
@@ -79,6 +94,46 @@ int main(int argc, char * argv[])
 		std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S %z", std::localtime(&tt));
 		model.set_doc_string("ONNX output generated from Darknet/YOLO neural network files " + cfg_fn.filename().string() + " and " + weights_fn.filename().string() + " on " + buffer + ".");
 
+		auto graph = new onnx::GraphProto();
+		graph->set_name(weights_fn.stem().string());
+
+		// https://github.com/onnx/onnx/blob/main/docs/Operators.md
+		const std::map<Darknet::ELayerType, std::string> op =
+		{
+			{Darknet::ELayerType::CONVOLUTIONAL	, "Conv"	},
+			{Darknet::ELayerType::ROUTE			, "Split"	},	// or "Concat"?
+			{Darknet::ELayerType::MAXPOOL		, "MaxPool"	},
+			{Darknet::ELayerType::UPSAMPLE		, "Upsample"},	// beware, this was deprecated starting with onnx operator set version 10
+		};
+
+		const std::set<Darknet::ELayerType> types_to_skip =
+		{
+			Darknet::ELayerType::YOLO,
+		};
+
+		for (size_t index = 0; index < cfg.sections.size(); index ++)
+		{
+			const auto & section = cfg.sections[index];
+			std::cout << "INDEX=" << (index + 1) << " LINE #" << section.debug() << std::endl;
+			if (types_to_skip.count(section.type))
+			{
+				continue;
+			}
+
+			auto node = graph->add_node();
+			std::stringstream ss;
+			ss << std::setfill('0') << std::setw(3) << (index + 1) << "_" << section.name;
+			node->set_name(ss.str());
+
+			if (op.count(section.type) != 1)
+			{
+				throw std::invalid_argument("section type \"" + section.name + "\" is not yet supported by this tool");
+			}
+			node->set_op_type(op.at(section.type));
+		}
+
+		model.set_allocated_graph(graph);
+
 		std::cout
 			<< "-> type name ............ " << model.GetTypeName()			<< std::endl
 			<< "-> opset import size .... " << model.opset_import_size()	<< std::endl
@@ -105,8 +160,7 @@ int main(int argc, char * argv[])
 		std::cout << "-> onnx saved to ........ " << onnx_fn.string() << std::endl;
 
 		google::protobuf::ShutdownProtobufLibrary();
-
-		Darknet::free_neural_network(net);
+		free_network(*net);
 	}
 	catch (const std::exception & e)
 	{
